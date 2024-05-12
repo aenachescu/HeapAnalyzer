87101e39fc92fb19705e72d571916bb628a0cbe7
Alin Enachescu <alinenachescu033@gmail.com>
2024-05-12 14:10:02 +0300

    implemented FieldStatistics::ToString

--- HeapAnalyzerLib/FieldStatistics.h
+++ HeapAnalyzerLib/FieldStatistics.h
@@ -2,9 +2,20 @@
 
 #include "MinMax.h"
 #include "MathUtils.h"
+#include "StringUtils.h"
+
+#include <algorithm>
 
 class FieldStatistics
 {
+public:
+    static constexpr size_t kFieldNameAlignment = std::max({
+        sizeof("Total"),
+        sizeof("Avg"),
+        sizeof("Min"),
+        sizeof("Max"),
+    }) - 1;
+
 public:
     FieldStatistics() = default;
     ~FieldStatistics() = default;
@@ -36,6 +47,48 @@ public:
     inline void Process(size_t num)
     {
         avg = Avg(total, num);
+    }
+
+    WH_string ToString(size_t identation, const char* separator, const char* name, size_t alignment) const
+    {
+        WH_string res;
+
+        std::format_to(
+            std::back_inserter(res),
+            "{:{}}{}{:{}} : {}{}",
+            ' ', identation,
+            "Total",
+            name, alignment - (sizeof("Total") - 1),
+            ::ToString(total, true),
+            separator);
+
+        std::format_to(
+            std::back_inserter(res),
+            "{:{}}{}{:{}} : {}{}",
+            ' ', identation,
+            "Avg",
+            name, alignment - (sizeof("Avg") - 1),
+            ::ToString(avg, true),
+            separator);
+
+        std::format_to(
+            std::back_inserter(res),
+            "{:{}}{}{:{}} : {}{}",
+            ' ', identation,
+            "Min",
+            name, alignment - (sizeof("Min") - 1),
+            ::ToString(min, true),
+            separator);
+
+        std::format_to(
+            std::back_inserter(res),
+            "{:{}}{}{:{}} : {}",
+            ' ', identation,
+            "Max",
+            name, alignment - (sizeof("Max") - 1),
+            ::ToString(max, true));
+
+        return res;
     }
 
     inline size_t GetTotal() const
